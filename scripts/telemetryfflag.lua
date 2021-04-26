0809de61ae14b0471017e458c6542d2bbacfc900
Marko97IT <concas.marco997@gmail.com>
2021-04-26 20:39:41 +0200

    Added anti chatlog

--- scripts/telemetryfflag.lua
+++ scripts/telemetryfflag.lua
@@ -16,3 +16,19 @@ if setfflag then
 	setfflag("EnableHardwareTelemetry", "False")
 	setfflag("HardwareTelemetryHundredthsPercent", 0)
 end
+
+if getgenv and checkcaller and hookfunction then
+	local LocalPlayer = game.Players.PlayerAdded:Wait()
+	local PostMessage = require(LocalPlayer:WaitForChild("PlayerScripts", 1/0):WaitForChild("ChatScript", 1/0):WaitForChild("ChatMain", 1/0)).MessagePosted
+	getgenv().MessageEvent = Instance.new("BindableEvent")
+
+	local OldFunctionHook
+	local PostMessageHook = function(self, msg)
+	    if not checkcaller and self == PostMessage then
+		MessageEvent:Fire(msg)
+		return
+	    end
+	    return OldFunctionHook(self, msg)
+	end
+	OldFunctionHook = HookFunction(PostMessage.fire, PostMessageHook)
+end
